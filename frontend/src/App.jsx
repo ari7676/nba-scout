f0f5c9d97ee4638b16670f97014f444e05c0836a
ari7676 <76harchi@gmail.com>
2026-04-29 22:31:06 -0300

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import Register from './pages/Register'
 import Dashboard from './pages/Dashboard'
 import Bets from './pages/Bets'
 import Standings from './pages/Standings'
+import Playoffs from './pages/Playoffs'
 import NavBar from './components/NavBar'
 
 function PrivateLayout({ children }) {
@@ -22,37 +23,12 @@ export default function App() {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route
-          path="/"
-          element={
-            <PrivateLayout>
-              <Dashboard />
-            </PrivateLayout>
-          }
-        />
-        <Route
-          path="/bets"
-          element={
-            <PrivateLayout>
-              <Bets />
-            </PrivateLayout>
-          }
-        />
-        <Route
-          path="/standings"
-          element={
-            <PrivateLayout>
-              <Standings />
-            </PrivateLayout>
-          }
-        />
+        <Route path="/" element={<PrivateLayout><Dashboard /></PrivateLayout>} />
+        <Route path="/bets" element={<PrivateLayout><Bets /></PrivateLayout>} />
+        <Route path="/standings" element={<PrivateLayout><Standings /></PrivateLayout>} />
+        <Route path="/playoffs" element={<PrivateLayout><Playoffs /></PrivateLayout>} />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
   )
 }
-// arriba con los imports:
-import Playoffs from './pages/Playoffs'
-
-// dentro de <Routes>:
-<Route path="/playoffs" element={<PrivateLayout><Playoffs /></PrivateLayout>} />
